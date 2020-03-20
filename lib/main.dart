15f25f39683acfe99b0006448c0822a82fd820f4
ValentedVishalParmar <vishalparmar9173@gmail.com>
2020-03-20 10:30:39 +0530

    name changed

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CRUD Demo',
+      title: 'CRUD Demo1',
       debugShowCheckedModeBanner: false,
       
       theme: ThemeData(
